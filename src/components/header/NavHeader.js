942f2f5551146d5e417be23339966a30b377e1e9
can7wysiwyg <paulkssa@gmail.com>
2023-06-25 18:44:19 +0200

    some final changes of the day

--- src/components/header/NavHeader.js
+++ src/components/header/NavHeader.js
@@ -1,31 +1,48 @@
+import { Link } from "react-router-dom";
+import "./header.css"
+
 function NavHeader() {
   return (
     <>
     <nav className="navbar navbar-expand-lg bg-dark">
-    <a className="navbar-brand" href="/">Your Logo</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/authors">Authors</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/books">Books</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/contact">Contact Us</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <Link className="navbar-brand" to="/">Your Logo</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/authors">Authors</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/books">Books</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Categories
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="/">Category 1</a>
+              <a className="dropdown-item" href="/">Category 2</a>
+              <a className="dropdown-item" href="/">Category 3</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">About Us</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <form className="form-inline search-form">
+          <input type="text" placeholder="Search" />
+        </form>
+      </div>
+    </nav>
+    
   
 
         </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,69 +1,66 @@
 import img1 from "../images/img1.jpg"
 import img2 from "../images/img2.jpg"
+import "animate.css/animate.css"
+import "./styles/home.css"
+import { Link } from 'react-router-dom';
+
+
 
 function Home() {
   return (
    < >
-   
    <header className="header-image">
-    <div className="header-text">
-      <h1 className="animate__animated animate__fadeInDown" >Discover the World of Audio Books</h1>
-      <p className="animate__animated animate__fadeInUp" >Explore captivating stories, immerse yourself in new adventures, and unlock knowledge with our extensive collection of audio books.</p>
-    </div>
-  </header>
-<br />
-  <section className="container">
-    <div className="row">
-      <div className="col-md-6">
-        <div className="animate__animated animate__fadeInLeft">
-          <img src={img1} alt="Image1" className="img-fluid" />
-        </div>
-      </div>
-      <div className="col-md-6">
-        <div className="animate__animated animate__fadeInRight">
-          <h2>Why Should Authors Advertise With Us?</h2>
-          <p>
+   <div className="header-text">
+  <h1 className="animated fadeInDown spin-animation">Discover the World of Audio Books</h1>
+</div>
 
+      </header>
 
-          "Are you an author looking to reach a wider audience and boost your book sales? 
-          Look no further! Advertising your books with us offers unparalleled benefits.
-           Our platform is specifically designed to connect authors with eager readers 
-           who are actively seeking new and captivating stories. With our targeted marketing strategies,
-            we ensure that your books reach the right audience, increasing your chances of success.
-             Our dedicated team of professionals will work closely with you to create compelling ad campaigns
+      <section className="container">
+        <div className="row">
+          <div className="col-md-6 animated fadeInLeft">
+            <img src={img1} alt="Image1" className="img-fluid" />
+          </div>
+          <div className="col-md-6 animated fadeInRight">
+            <h2>Why Should Authors Advertise With Us?</h2>
+            <p>
+              "Are you an author looking to reach a wider audience and boost your book sales? 
+              Look no further! Advertising your books with us offers unparalleled benefits.
+              Our platform is specifically designed to connect authors with eager readers 
+              who are actively seeking new and captivating stories. With our targeted marketing strategies,
+              we ensure that your books reach the right audience, increasing your chances of success.
+              Our dedicated team of professionals will work closely with you to create compelling ad campaigns
               that showcase the unique essence of your books. Gain exposure, build your author brand,
-               and watch your readership soar by advertising with us!"
-          </p>
+              and watch your readership soar by advertising with us!"
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-md-6">
-        <div className="animate__animated animate__fadeInLeft">
-          <h2>Why Should You Read?</h2>
-          <p>
-          "Immerse yourself in the enchanting world of books and unlock a myriad of possibilities. 
-          Reading is not just a hobby; 
-          it's a transformative experience that broadens your horizons and enriches your life.
-           With books, 
-           you can travel to far-off lands,
-            explore diverse cultures,
-            and embark on thrilling adventures—all from the comfort of your favorite reading spot. 
-            Books inspire, educate, and ignite the imagination,
-             allowing you to escape the mundane and discover new perspectives."
-
-          </p>
+        <div className="row">
+          <div className="col-md-6 animated fadeInLeft">
+            <h2>Why Should You Read?</h2>
+            <p>
+              "Immerse yourself in the enchanting world of books and unlock a myriad of possibilities. 
+              Reading is not just a hobby; 
+              it's a transformative experience that broadens your horizons and enriches your life.
+              With books, 
+              you can travel to far-off lands,
+              explore diverse cultures,
+              and embark on thrilling adventures—all from the comfort of your favorite reading spot. 
+              Books inspire, educate, and ignite the imagination,
+              allowing you to escape the mundane and discover new perspectives."
+            </p>
+          </div>
+          <div className="col-md-6 animated fadeInRight">
+            <img src={img2} alt="Image2" className="img-fluid" />
+          </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        <div className="animate__animated animate__fadeInRight">
-          <img src={img2} alt="Image2" className="img-fluid" />
+        <br />
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <Link to="/books" className="btn btn-warning">Browse Books</Link>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
-  <br />
-
+      </section>
       
     </>
   );
