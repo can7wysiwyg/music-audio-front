192fe28ada8c30980b4dc0712d766fdd1dd0811a
can7wysiwyg <paulkssa@gmail.com>
2023-06-28 15:19:18 +0200

    adding categories to menu

--- src/components/header/NavHeader.js
+++ src/components/header/NavHeader.js
@@ -1,145 +1,105 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GlobalState } from "../../GlobalState";
-import "./header.css"
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import "./header.css";
+import axios from "axios";
 
 function NavHeader() {
   const state = useContext(GlobalState);
-    const [isLogged] = state.userApi.isLogged;
-  const [isAdmin] = state.userApi.isAdmin; 
+  const [isLogged] = state.userApi.isLogged;
+  const [isAdmin] = state.userApi.isAdmin;
+  const [categories, setCategories] = useState([]);
 
-  const logoutUser = async () => {
-    localStorage.removeItem('token')
+useEffect(() => {
 
-     
-    window.location.href = "/";
-
-  
-
+  const getGenres = async () => {
+    const res = await axios.get('/genre/show_all');
+    setCategories(res.data.results);
   };
 
-function Webname() {
-  if(isLogged !== true) {
-    return(<>
-    <nav>
-    <Link className="navbar-brand" to="/">Your Logo</Link>
-    </nav>
-    
-    </>)
+  getGenres();
 
-  } else{
 
-    return(<>
-    <nav>
-    <Link className="navbar-brand" to="/">Admin</Link>
 
-    </nav>
-    </>)
+}, [])  
 
+  const logoutUser = async () => {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
+  function Webname() {
+    if (isLogged !== true) {
+      return (
+        <Link className="navbar-brand" to="/">
+          Your Logo
+        </Link>
+      );
+    } else {
+      return (
+        <Link className="navbar-brand" to="/">
+          Admin
+        </Link>
+      );
+    }
   }
-  
-}
-
 
   const loggedRouter = () => {
     return (
-        <nav>
-          
-        
-            <Link className="navbar-brand" to="/" onClick={logoutUser}>
-            Logout
-            </Link>
-          
-        </nav>
-      );
-
-  }
-
-  
+      <Nav>
+        <Link className="nav-link" to="/" onClick={logoutUser}>
+          Logout
+        </Link>
+      </Nav>
+    );
+  };
 
   const AdminRoute = () => {
-    return(<>
-    <nav>
-    
-    <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/admin_panel">Management</Link>
-          </li>
-          </ul>
-
-    </nav>
-    
-    </>)
-  }
-
+    return (
+      <Nav>
+        <NavDropdown title="Management" id="basic-nav-dropdown">
+          <NavDropdown.Item href="/admin_panel">Admin Panel</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    );
+  };
 
   const AdminLogin = () => {
-    if(isAdmin) {
-      return AdminRoute()
-  }   
-
-  }
-
-
-
+    if (isAdmin) {
+      return AdminRoute();
+    }
+  };
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-dark">
-      {Webname()}
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        {AdminLogin()}
-        <ul className="navbar-nav ml-auto">
-
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/authors">Authors</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/books">Books</Link>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Categories
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/">Category 1</a>
-              <a className="dropdown-item" href="/">Category 2</a>
-              <a className="dropdown-item" href="/">Category 3</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About Us</Link>
-          </li>
-    
-        
-
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact Us</Link>
-          </li> 
-        </ul>
-        <form className="form-inline search-form">
-          <input type="text" placeholder="Search" />
-        </form>
-      </div>
-
-      {
-        isLogged ? (
-          loggedRouter()
-        ) : (
-          ""
-        )
-      }
-    </nav>
-    
-  
-
-        </>
+      <Navbar bg="dark" expand="lg">
+        {Webname()}
+        <Navbar.Toggle aria-controls="navbarNav" />
+        <Navbar.Collapse id="navbarNav">
+          {AdminLogin()}
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/authors">Authors</Nav.Link>
+            <Nav.Link href="/books">Books</Nav.Link>
+            <NavDropdown title="Book Genres" id="basic-nav-dropdown">
+              {
+                categories?.map((category) => {
+                  return  <NavDropdown.Item href="/" key={category._id}>{category.bookGenre}</NavDropdown.Item>
+                })
+              } 
+            </NavDropdown>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
+          </Nav>
+          <Form inline className="search-form">
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+          {isLogged ? loggedRouter() : ""}
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
